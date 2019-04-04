c2f3e81bd5a44aa98cc6ab434cbdcf2f95ab6f64
Kevin Edzenga <trancor@metal-asylum.net>
2019-04-04 13:17:49 -0400

    Initial Release
    Initial Release

---
+++ README.md
@@ -0,0 +1,15 @@
+# **Line Draw**  *v0.0.1 - Unreleased*
+## **First C++ Program & Initial Test For OpenFrameworks**
+#### *Move your mouse around, it will effect the movement of the line*
+
+______________________________
+
+Decided to get OpenFrameworks to help me learn C++ along with the ability to connect to USB peripherals, webcam, among many other things.
+
+So, this is my first compiled program!
+
+I'm storing some point positions, then drawing multi colored triangles between neighboring points to create a trippy trail!
+
+______________________________
+
+## Thanks for checking out pxlTextGenerator!
--- visualStudio/ofApp.cpp
+++ visualStudio/ofApp.cpp
@@ -44,7 +44,10 @@ void ofApp::update() {
 }
 
 //--------------------------------------------------------------
-
+// Don't mind the unused functions
+// Just needed some basic math
+// OpenFramework prob has these, along with the math.h
+// Guess I'm lazy for looking around, more hands on to just go! hah
 void delta(float *arr, float px1, float py1, float px2, float py2) {
 	float distX = px2 - px1;
 	float distY = py2 - py1;
@@ -87,9 +90,9 @@ float sign(float val) {
 	return ret;
 }
 
+//--------------------------------------------------------------
 void ofApp::draw() {
 	ofBackgroundGradient(ofColor(0, 0, 0), ofColor(10, 19, 31), OF_GRADIENT_CIRCULAR);
-	ofSetColor(255, 0, 0);
 
 	int curX = sW * .5;
 	int curY = sH * .5;
@@ -100,23 +103,31 @@ void ofApp::draw() {
 	caps = min(1., max(0., cos(absRunner*.01) * 3 + .5));
 
 	for (int x = 0; x < runStop; ++x) {
+		// Get percentages rather than absolute values
+		// So I can change the point count without effecting the look and feel of the line
 		float rate = ((float)x / (float(runStop) - 1));
-		float fadeMult = 5;
-		double blend = min(float(rate*fadeMult*.5), float(fadeMult - (rate*fadeMult)));
-		blend = max(0.0, min(1.0, blend));
-		fadeMult = 50;
+		// Fade the tips of the line
+		// It didn't look good with the ends visible
+		// Using this for color effects
+		float fadeMult = 50;
 		double blendEnd = min(rate*fadeMult, float(fadeMult - (rate*fadeMult)));
 		blendEnd = max(0.0, min(1.0, blendEnd));
 
 		float mouseBlend = 1 - (abs(mouseXNorm - .5) / .5);
 
-		float mouseXSign = float((0 < (mouseXNorm - .5)) - ((mouseXNorm - .5) < 0));
-		if (mouseXSign == 0) {
-			mouseXSign = 1;
-		}
+		// Not using this anymore, there is prob one in OpenFramework but just wrote by own
+		float mouseXSign = sign(mouseXNorm - .5);
+
+		// Current X Position
+		// caps is changing the values between how Red and Blue mode looks.
+		float freqOne = (sin(rate*xMult*.5 + runner * swMult + cos(rate + .1) + mouseXNorm) * (mouseXNorm * 2 - 1) + (1. - mouseXNorm * .5))*(1 - caps);
 		float freqTwo = sin(float(x)*.004 + runner * swMult*.1*min(1., max(-1., (mouseXNorm - .5)*2.))) * 1 * caps;
-		int toX = (int)(((sin(rate*xMult*.5 + runner * swMult + cos(rate + .1) + mouseXNorm) * (mouseXNorm * 2 - 1) + (1. - mouseXNorm * .5))*(1 - caps) + freqTwo) * ((float)sW*sin(rate*xMult + runner * swMult*.7)*.3 + mouseXNorm * .1) + sW * .5);
-		int toY = (int)((cos(rate*xMult + curTimer * swMult*.7 + (mouseYNorm - .5) * 3) + sin(rate*PI + PI) + sin(mouseXNorm*PI) + cos(mouseYNorm*PI))  * (float)sH*.5 * sin(rate + float(x + 15.15)*.001*(.1*(mouseYNorm) * 4 + .7))*.4 + (float)sH*.5);
+		int toX = (int)((freqOne + freqTwo) * ((float)sW*sin(rate*xMult + runner * swMult*.7)*.3 + mouseXNorm * .1) + sW * .5);
+
+		// Current Y Position
+		freqOne = (cos(rate*xMult + curTimer * swMult*.7 + (mouseYNorm - .5) * 3) + sin(rate*PI + PI) + sin(mouseXNorm*PI) + cos(mouseYNorm*PI));
+		freqTwo = (float)sH*.5 * sin(rate + float(x + 15.15)*.001*(.1*(mouseYNorm) * 4 + .7))*.4;
+		int toY = (int)(freqOne * freqTwo + (float)sH*.5);
 
 		if (x < lineCountPrev) {
 			float xyPush[2] = {};
@@ -125,11 +136,14 @@ void ofApp::draw() {
 			normalize(xyPush);
 			dist = max(0., min(1., dist / sqrt(sW*sW + sH * sH)));
 
+			// Some Debugging Code
+			// Display text on screen in window
 			/*if (x == int(runStop*.9)) {
 				ofSetColor(255, 0, 0);
 				ofDrawBitmapString(ofToString(x) + " -- " + ofToString(dist) + " -- " + ofToString(xyPush[0]) + " -- " + ofToString(xyPush[1]) + " -- " + ofToString(toX) + " -- " + ofToString(sW*.5), 20, 20);
 			}*/
 
+			// Current triangle color--
 			float R = 255 * (1 - abs(sin(rate*xMult + runner * swMult*.5))) * (1 - caps) + 5 * caps;
 			float G = 80 * (1 - min(1., abs(sin(rate*xMult + runner * swMult*.5))*1.2))*rate - 30 * caps;
 			float B = 90 * caps + 165 * (1 - max(0., abs(sin(rate*xMult*2. + runner * swMult)*1.2 - .2))) * caps;
@@ -137,20 +151,20 @@ void ofApp::draw() {
 			ofSetColor(R, G, B, A);
 
 
-
+			// Draw the triangles! 2 per group
 			ofDrawTriangle(xPosArr[x][1], yPosArr[x][1], toX, toY, curX, curY);
+			// Wanted neightboring point positions, +1 looked better than -1 to me
+			// So can't read past the last point position
 			if (x < runStop - 1) {
 				ofDrawTriangle(xPosArr[x][1], yPosArr[x][1], toX, toY, xPosArr[x + 1][1], yPosArr[x + 1][1]);
 			}
-			//
-			/*
-			ofDrawTriangle(xPosArr[x][0], yPosArr[x][0], toX, toY, xPosArr[x][1], yPosArr[x][1]);
-			if (x < runStop - 1) {
-				ofDrawTriangle(xPosArr[x+1][0], yPosArr[x+1][0], xPosArr[x][1], yPosArr[x][1] , xPosArr[x + 1][1], yPosArr[x + 1][1]);
-			}
-			*/
+
 			curX = toX;
 			curY = toY;
+
+			// Points that are farther away from the center of screen
+			// Push them farther out slightly
+			// caps is changing the values between how Red and Blue mode looks.
 			float toXOrig = (toX - sW * .5);
 			float toYOrig = (toY - sH * .5);
 			toXOrig = max(0., abs(toXOrig) - 1.) * sign(toXOrig) * (1 + (1 + sin(rate*PI * 2 + curTimer * .6 + (toX - sW * .5) / sW * PI * 2 + (toY - sH * .5) / sH * PI * 2))*(1 - caps))*.1;
